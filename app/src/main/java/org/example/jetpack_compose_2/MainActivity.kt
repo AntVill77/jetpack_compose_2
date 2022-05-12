e195ae5062ce456d81aba1243a6e050f7ecbaf96
AntVill77 <antvillot77@hotmail.com>
2022-05-12 02:06:20 -0500

    primer ejercicio

--- app/src/main/java/org/example/jetpack_compose_2/MainActivity.kt
+++ app/src/main/java/org/example/jetpack_compose_2/MainActivity.kt
@@ -3,12 +3,16 @@ package org.example.jetpack_compose_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import org.example.jetpack_compose_2.ui.theme.Jetpack_compose_2Theme
 
@@ -16,28 +20,42 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Jetpack_compose_2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyComponent()
         }
+
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Jetpack_compose_2Theme {
-        Greeting("Android")
+fun MyComponent(){
+    Row {
+        MyImage()
+        MyTexts()
     }
 }
+@Composable
+fun MyImage(){
+    Image(
+        painterResource(R.drawable.ic_launcher_foreground),
+              "Mi imagen de prueba"
+        )
+}
+@Composable
+fun MyTexts(){
+    Column() {
+        MyText("Hola Hola")
+        MyText("Mundi mundo")
+
+    }
+}
+@Composable
+fun MyText(text: String){
+    Text(text)
+}
+
+@Preview
+@Composable
+fun PreviewComponent(){
+    MyComponent()
+}
+
+
