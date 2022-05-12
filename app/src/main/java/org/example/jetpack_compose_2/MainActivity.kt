373eb3d5e454f4a3c460a3005eaadc768ef2e616
AntVill77 <antvillot77@hotmail.com>
2022-05-12 15:37:47 -0500

    ejercicio2

--- app/src/main/java/org/example/jetpack_compose_2/MainActivity.kt
+++ app/src/main/java/org/example/jetpack_compose_2/MainActivity.kt
@@ -1,33 +1,45 @@
 package org.example.jetpack_compose_2
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.example.jetpack_compose_2.ui.theme.Jetpack_compose_2Theme
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComponent()
+            Jetpack_compose_2Theme() {
+                MyComponent()
+            }
+
         }
 
     }
 }
 @Composable
 fun MyComponent(){
-    Row {
+    Row (modifier = Modifier
+        .background(MaterialTheme.colors.background)
+        .padding(8.dp)
+    )
+    {
         MyImage()
         MyTexts()
     }
@@ -36,26 +48,44 @@ fun MyComponent(){
 fun MyImage(){
     Image(
         painterResource(R.drawable.ic_launcher_foreground),
-              "Mi imagen de prueba"
+              "Mi imagen de prueba",
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(MaterialTheme.colors.primary)
+            .size(64.dp)
         )
 }
 @Composable
 fun MyTexts(){
-    Column() {
-        MyText("Hola Hola")
-        MyText("Mundi mundo")
+    Column(modifier = Modifier.padding(start = 8.dp)) {
+        MyText("Hola Hola",
+                MaterialTheme.colors.primaryVariant,
+
+            )
+        Spacer(modifier = Modifier.height(16.dp))
+        MyText("Mundo mundo",
+        MaterialTheme.colors.onBackground,
+
+
+            )
 
     }
 }
 @Composable
-fun MyText(text: String){
-    Text(text)
+fun MyText(text: String, color: Color){
+    Text(text, color = color)
 }
 
-@Preview
+
+
+@Preview()
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewComponent(){
-    MyComponent()
+    Jetpack_compose_2Theme() {
+        MyComponent()
+    }
+
 }
 
 
